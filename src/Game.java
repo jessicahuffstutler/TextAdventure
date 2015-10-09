d8c6f42ff186c978ba8e9b5fa44d18b87bc7cfa3
jessicahuffstutler <jessica.huffstutler@gmail.com>
2015-10-09 11:14:48 -0400

    Start command system

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created by jessicahuffstutler on 10/8/15.
  */
@@ -6,8 +8,22 @@ public class Game {
         System.out.println("Welcome to my text adventure.");
 
         Player player = new Player(); //how to create the player
-        player.chooseName(); //call those three methods from Player.java
-        player.chooseWeapon(); //call those three methods from Player.java
-        player.chooseArea(); //call those three methods from Player.java
+        player.chooseName(); //call those three methods from Player class
+        player.chooseWeapon(); //call those three methods from Player class
+        player.chooseArea(); //call those three methods from Player class
+    }
+
+    static String nextLine() { //new method, like a utility function, not public bc we will not call this from the JREPL and needs to be static so it can be called anytime.
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        if (s.startsWith("/")) {
+            if (s.equals("/help")) {
+                System.out.println("These are the available commands:");
+                System.out.println("/help => List available commands");
+            }
+            return nextLine(); //call a function inside of itself; it's going to call itself again and read itself again and if it's a forward slash, it will do it all again until it's not a forward slash
+        } else {
+            return s;
+        }
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -10,16 +10,14 @@ public class Player {
 
     void chooseName(){ //Merric, when do we use curly braces and when to use semi colon?
         System.out.println("What is your name, traveler?");
-        Scanner scanner = new Scanner(System.in);
-        name = scanner.nextLine(); //get rid of where it said "String name = scanner.nextLine();" and just put "name = scanner.nextLine();" to save the name and have it call the name from the field above
+        name = Game.nextLine(); //replaced two lines with this so that we do not have to continuously call on the scanner; we also did this below
         System.out.println("Good luck, " + name);
     }
 
-    void chooseWeapon() throws Exception {
+    void chooseWeapon() throws Exception { //method
         System.out.println("[1] Pick up a sword");
         System.out.println("[2] Pick up a mallet");
-        Scanner scanner = new Scanner(System.in);
-        weapon = scanner.nextLine(); //we removed the first word "String" as above so that we are using the weapon field and it is storing what weapon we chose
+        weapon = Game.nextLine();
         int weaponNum = Integer.valueOf(weapon); //the argument is a string, it will return a number
         if (weaponNum == 1) {
             System.out.println("That's a fine sword!");
@@ -30,11 +28,10 @@ public class Player {
         }
     }
 
-    void chooseArea() throws Exception {
+    void chooseArea() throws Exception { //method
         System.out.println("[1] Enter the forest");
         System.out.println("[2] Enter the underground tunnel");
-        Scanner scanner = new Scanner(System.in);
-        area = scanner.nextLine(); //remove "String" (before area) as above so it saves it to that field
+        area = Game.nextLine();
         int areaNum = Integer.valueOf(area);
         if (area.equals("1")) {
             System.out.println("Entering the forest...");
